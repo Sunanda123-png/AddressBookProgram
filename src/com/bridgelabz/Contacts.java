6a1d78cbc3501bf57b76a248f2d90c4af7f6454c
Sunanda123-png <sunanda02@gmail.com>
2021-10-04 01:13:05 +0530

    add Commenting

--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -1,13 +1,16 @@
 package com.bridgelabz;
 
-import java.math.BigInteger;
+/**
+ * Author:-Sunanda Shil
+ * date:-03/10/2021
+ */
 
 public class Contacts {
 
     private String firstName, lastName, city, state, email;
     private int zip;
     private String phoneNumber;
-
+//creating constructor
     public Contacts(String firstName, String lastName, String city, String state, int zip, String email,
                    String phoneNumber) {
         this.firstName = firstName;
@@ -18,8 +21,7 @@ public class Contacts {
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
-
-
+//to display the output
     public void display()
     {
         System.out.println("First Name : "+firstName+", Last Name : "+lastName+", City : "+city+", State : "+state+", Zip code : "+zip+", Email-ID : "+email+", Phone Number : "+phoneNumber);
@@ -28,6 +30,7 @@ public class Contacts {
 }
 class AddressBook
 {
+    //creating object to call constructor
     public static void main(String[] args)
     {
         Contacts address=new Contacts("Sunanda","Shil","Guwahati","Assam",784115,"ssunanda02@gmail.com","7578061886");
