8dad0be1a975e2ba660fb1ee304614d78dae8daa
Sunanda123-png <sunanda02@gmail.com>
2021-10-04 23:25:22 +0530

    Adding Edit details code

--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
 
 public class Contacts
 {
+
+    private static Contact contact;
+
     public static void addPerson()
     {
         ArrayList<String> firstName= new ArrayList<>();
@@ -41,6 +44,68 @@ public class Contacts
         System.out.println("First Name : "+firstName+", Last Name : "+lastname+", City : "+city+", State : "+State+", Zip code : "+Zip+", Email-ID : "+Email+", Phone Number : "+phoneNo);
         
     }
+    public static void editPerson() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name you want to edit...");
+        String editName = scanner.next();
+        System.out.println("What do you want to edit \n" +
+                "1. First Name / 2. Last Name / 3. Address / 4. City / " +
+                "5. State / 6. Zip code / 7. Phone Number / 8. Email");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Enter new First Name");
+                String newFirstName = scanner.next();
+                contact.setFirstName(newFirstName);
+                break;
+
+            case 2:
+                System.out.println("Enter new Last Name");
+                String newLastName = scanner.next();
+                contact.setLastName(newLastName);
+                break;
+
+            case 3:
+                System.out.println("Enter new Address");
+                String newAddress = scanner.next();
+                contact.setAddress(newAddress);
+                break;
+
+            case 4:
+                System.out.println("Enter new City");
+                String newCity = scanner.next();
+                contact.setCity(newCity);
+                break;
+
+            case 5:
+                System.out.println("Enter new State");
+                String newState = scanner.next();
+                contact.setState(newState);
+                break;
+
+            case 6:
+                System.out.println("Enter new Zip code");
+                int newZip = scanner.nextInt();
+                contact.setZip(newZip);
+                break;
+
+            case 7:
+                System.out.println("Enter new Phone Number");
+                long newPhoneNo = scanner.nextLong();
+                contact.setPhoneNo(newPhoneNo);
+                break;
+
+            case 8:
+                System.out.println("Enter new Email");
+                String newEmail = scanner.next();
+                contact.setEmail(newEmail);
+                break;
+        }
+    }
+
+
+
 
 
         //calling function from main
@@ -48,5 +113,6 @@ public class Contacts
     {
         System.out.println("Welcome to Address Book System System.");
         addPerson();
+        editPerson();
     }
 }
