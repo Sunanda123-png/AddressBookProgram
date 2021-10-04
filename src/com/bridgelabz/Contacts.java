ba1af8b9c738c682c83396db742ae68809a9cfc6
Sunanda123-png <sunanda02@gmail.com>
2021-10-04 21:17:46 +0530

    Adding new pwerson

--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -1,41 +1,52 @@
 package com.bridgelabz;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Author:-Sunanda Shil
  * date:-03/10/2021
  */
 
-public class Contacts {
-
-    private String firstName, lastName, city, state, email;
-    private int zip;
-    private String phoneNumber;
-//creating constructor
-    public Contacts(String firstName, String lastName, String city, String state, int zip, String email,
-                   String phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-//to display the output
-    public void display()
-    {
-        System.out.println("First Name : "+firstName+", Last Name : "+lastName+", City : "+city+", State : "+state+", Zip code : "+zip+", Email-ID : "+email+", Phone Number : "+phoneNumber);
-    }
-
-}
-class AddressBook
+public class Contacts
 {
-    //creating object to call constructor
+    public static void addPerson()
+    {
+        ArrayList<String> firstName= new ArrayList<>();
+        ArrayList<String> lastname= new ArrayList<>();
+        ArrayList<String> Address= new ArrayList<>();
+        ArrayList<String> city= new ArrayList<>();
+        ArrayList<String> State= new ArrayList<>();
+        ArrayList<Integer> Zip= new ArrayList<>();
+        ArrayList<Long> phoneNo= new ArrayList<>();
+        ArrayList<String> Email= new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter following details \n" +
+                "First Name :");
+        firstName.add(scanner.next());
+        System.out.println("Last Name :");
+        lastname.add(scanner.next());
+        System.out.println("Address :");
+        Address.add(scanner.next());
+        System.out.println("City :");
+        city.add(scanner.next());
+        System.out.println("State :");
+        State.add(scanner.next());
+        System.out.println("Zip Code :");
+        Zip.add(scanner.nextInt());
+        System.out.println("Phone Number :");
+        phoneNo.add(scanner.nextLong());
+        System.out.println("Email :");
+        Email.add(scanner.next());
+        System.out.println("First Name : "+firstName+", Last Name : "+lastname+", City : "+city+", State : "+State+", Zip code : "+Zip+", Email-ID : "+Email+", Phone Number : "+phoneNo);
+        
+    }
+
+
+        //calling function from main
     public static void main(String[] args)
     {
-        Contacts address=new Contacts("Sunanda","Shil","Guwahati","Assam",784115,"ssunanda02@gmail.com","7578061886");
-        address.display();
-
+        System.out.println("Welcome to Address Book System System.");
+        addPerson();
     }
-
 }
